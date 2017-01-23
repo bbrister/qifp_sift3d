393de031460e8a44f93611032dd1890ee4a8e9a6
Blaine Rister <blaine@stanford.edu>
2017-01-23 14:04:42 -0800

    add driver script

---
+++ siftfeature.sh
@@ -0,0 +1,10 @@
+# Usage: siftfeature.sh [dso filename] [output filename]
+DSO=$1
+OUTPUT=$2
+PROG=build/bin/sift3d_qifp
+# Run the program
+$PROG $DSO $OUTPUT
+# Remove the first 3 columns of the output, which give the feature coordinates
+TMP=mktemp || exit 1
+cut -d, -f4- $OUTPUT > $TMP
+cat $TMP > $OUTPUT
